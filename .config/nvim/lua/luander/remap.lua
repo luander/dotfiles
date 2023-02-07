f00ac15fed30c382bbbfde7033f6033c067e8ad1
luander <iam@luander.net>
2023-02-07 20:38:41 +0100

    Add rust nvim plugin, change ssh hosts

--- .config/nvim/lua/luander/remap.lua
+++ .config/nvim/lua/luander/remap.lua
@@ -1,39 +1,27 @@
--- map jk to <Esc> when in insert mode
-vim.keymap.set("i", "jk", "<Esc>")
-
--- map : to ;
-vim.keymap.set("n", ";", ":")
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- greatest remap ever
-vim.keymap.set("x", "<leader>p", "\"_dP")
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("i", "jk", "<Esc>", { desc = "jk combination take us to normal mode" })
+vim.keymap.set("n", ";", ":", { desc = "semicolon is colon when in normal mode" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Shifts a whole selected block one line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Shifts a whole selected block one line up" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Ctrl+d is page down and center screen" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Ctrl+d is page up and center screen" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Searches for next match and center screen" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Searches for previous match and center screen" })
+vim.keymap.set("x", "<leader>p", "\"_dP", { desc = "" })
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "Add current yanked text to buffer" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Add current yanked text to buffer" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Add current yanked text to buffer" })
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Not sure " })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Not sure " })
+vim.keymap.set("n", "Q", "<nop>", { desc = "Removes any command from Q" })
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Not sure, create tmux tab" })
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
-end)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+end, { desc = "Format current file" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Go to next error" })
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Go to previous error" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Go to next location" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Go to previous location" })
+vim.keymap.set("n", "*", "*zz", { desc = "search and center screen" })
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+    { desc = "Live text substitution throughout the whole file" })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make current file executable" })
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -130,6 +130,7 @@ _G.packer_plugins = {
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["neo-tree.nvim"] = {
+    config = { "\27LJ\2\n«\1\0\0\5\0\v\0\0156\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\3\0'\2\4\0B\0\2\0029\0\5\0005\2\t\0005\3\a\0005\4\6\0=\4\b\3=\3\n\2B\0\2\1K\0\1\0\15filesystem\1\0\0\19filtered_items\1\0\0\1\0\2\fvisible\2\16hide_hidden\1\nsetup\rneo-tree\frequire0 let g:neo_tree_remove_legacy_commands = 1 \bcmd\bvim\0" },
     loaded = true,
     path = "/Users/lribeiro/.local/share/nvim/site/pack/packer/start/neo-tree.nvim",
     url = "https://github.com/nvim-neo-tree/neo-tree.nvim"
@@ -175,6 +176,11 @@ _G.packer_plugins = {
     path = "/Users/lribeiro/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
+  ["rust-tools.nvim"] = {
+    loaded = true,
+    path = "/Users/lribeiro/.local/share/nvim/site/pack/packer/start/rust-tools.nvim",
+    url = "https://github.com/simrat39/rust-tools.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/lribeiro/.local/share/nvim/site/pack/packer/start/telescope.nvim",
@@ -202,6 +208,10 @@ time([[Defining packer_plugins]], false)
 time([[Config for rose-pine]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
 time([[Config for rose-pine]], false)
+-- Config for: neo-tree.nvim
+time([[Config for neo-tree.nvim]], true)
+try_loadstring("\27LJ\2\n«\1\0\0\5\0\v\0\0156\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\3\0'\2\4\0B\0\2\0029\0\5\0005\2\t\0005\3\a\0005\4\6\0=\4\b\3=\3\n\2B\0\2\1K\0\1\0\15filesystem\1\0\0\19filtered_items\1\0\0\1\0\2\fvisible\2\16hide_hidden\1\nsetup\rneo-tree\frequire0 let g:neo_tree_remove_legacy_commands = 1 \bcmd\bvim\0", "config", "neo-tree.nvim")
+time([[Config for neo-tree.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
