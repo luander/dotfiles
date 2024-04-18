f8fe8d22dbcf6f194aa185537939f5fe53cd1b4e
luander <837720+luander@users.noreply.github.com>
2024-04-18 20:35:15 +0200

    Added neovim >0.10 support for inlay hints and 1password for ssh

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
 -- -- Disable autoformat for lua files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "toml" },
   callback = function()
     vim.b.autoformat = false
+  end,
+})
+
+-- -- Enable inlay hints on load
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(args.buf, true)
+    end
   end,
 })
