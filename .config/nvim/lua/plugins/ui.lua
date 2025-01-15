5188bd192532367a98003fab0379455ee9ad5742
luander <837720+luander@users.noreply.github.com>
2025-01-15 22:35:44 +0100

    Removes unused include in .ssh. Customizes nvim lualine

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,28 @@
 return {
+  -- Make file path length longer.
+  -- By default, LazyVim will truncate the path if it's more than 3 segments.
+  -- I find this too short, so I'm setting it to 7, which should show the full path in most cases.
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = LazyVim.config.icons.diagnostics.Error,
+              warn = LazyVim.config.icons.diagnostics.Warn,
+              info = LazyVim.config.icons.diagnostics.Info,
+              hint = LazyVim.config.icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path({ length = 7 }) },
+        },
+      },
+    },
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
