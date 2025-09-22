d29d14384e92cedddccc76cd907b23f585c67b05
luander <837720+luander@users.noreply.github.com>
2025-09-22 10:25:59 +0200

    Updates Lazyvim and aws hosts configuration

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -7,13 +7,4 @@ return {
       vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
     end,
   },
-  -- Ensure Rust debugger is installed
-  {
-    "williamboman/mason.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "codelldb", "cpptools" })
-    end,
-  },
 }
